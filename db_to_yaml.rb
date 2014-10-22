c26350b0365bfae2b0e985649dcea5a0f80d088c
miyohide <miyohide@gmail.com>
2014-10-22 22:21:52 +0900

    PostgreSQLに格納した住所情報を出力する

---
+++ db_to_yaml.rb
@@ -0,0 +1,40 @@
+require 'yaml'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+   adapter: 'postgresql',
+   host:    'localhost',
+   username: 'user1',
+   password: 'user1',
+   database: 'ken_all'
+)
+
+class Todofuken < ActiveRecord::Base
+end
+
+class City < ActiveRecord::Base
+end
+
+class Town < ActiveRecord::Base
+end
+
+todofuken = Todofuken.order(:id)
+city = City.order(:id)
+town = Town.order(:id)
+
+File.open("addresses.yml", "w") { |f|
+   f.write("addresses:\n")
+   f.write("  prefectural:\n")
+   todofuken.each do |record|
+      f.write("    - ['#{record.todofuken_kanji}', '#{record.todofuken_hira}', '#{record.todofuken_kana}']\n")
+   end
+   f.write("  city:\n")
+   city.each do |record|
+      f.write("    - ['#{record.city_kanji}', '#{record.city_hira}', '#{record.city_kana}']\n")
+   end
+   f.write("  town:\n")
+   town.each do |record|
+      f.write("    - ['#{record.town_kanji}', '#{record.town_hira}', '#{record.town_kana}']\n")
+   end
+}
+
