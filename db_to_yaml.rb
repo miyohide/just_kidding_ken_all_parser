efb36b9c4c4c03fcff523fcbb086c51ae2bf6a15
miyohide <miyohide@gmail.com>
2014-12-31 23:28:08 +0900

    Output todocuken/city_number column

--- db_to_yaml.rb
+++ db_to_yaml.rb
@@ -20,22 +20,22 @@ end
 
 todofuken = Todofuken.order(:id)
 city = City.order(:id)
-town = Town.where("id % 100 = 0").order(:id)
+town = Town.order(:id)
 
 File.open("addresses.yml", "w") { |f|
    f.write("addresses:\n")
    f.write("  prefectural:\n")
    todofuken.each do |record|
-      f.write("    - ['#{record.todofuken_kanji}', '#{record.todofuken_hira}', '#{record.todofuken_kana}']\n")
+      f.write("    - ['#{record.todofuken_kanji}', '#{record.todofuken_hira}', '#{record.todofuken_kana}', #{record.todofuken_number}]\n")
    end
    f.write("  city:\n")
    city.each do |record|
-      f.write("    - ['#{record.city_kanji}', '#{record.city_hira}', '#{record.city_kana}']\n")
+      f.write("    - ['#{record.city_kanji}', '#{record.city_hira}', '#{record.city_kana}', #{record.todofuken_number}, #{record.city_number}]\n")
    end
    f.write("  town:\n")
    town.each do |record|
       if record.town_kanji != '以下に掲載がない場合'
-         f.write("    - ['#{record.town_kanji}', '#{record.town_hira}', '#{record.town_kana}']\n")
+         f.write("    - ['#{record.town_kanji}', '#{record.town_hira}', '#{record.town_kana}', #{record.todofuken_number}, #{record.city_number}]\n")
       end
    end
 }
